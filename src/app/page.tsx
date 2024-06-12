117fcfb4e07b925fd68c4a1190db68a79bd3ebe2
Joono Chakma <73777352+joonopohr@users.noreply.github.com>
2024-06-13 00:09:52 +1000

    update the placement of components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,18 @@ export default function Home() {
   return (
     <main>
       <Header />
-      Hey. Im
+      <p className=" font-thin font-Inter px-52 py-9">Hey. Im</p>
       <Navbar />
-      I'm a passionate software developer based in Melbourne, Australia.
-      Currently, I am honing my skills and expanding my knowledge at Swinburne
-      University of Technology. My journey into the world of software
-      development is fueled by a deep curiosity and a love for technology. When
-      I'm not coding, you can find me capturing moments through photography or
-      creating melodies with my music. Welcome to my portfolio, where I showcase
-      my projects and the creative solutions I've developed along the way.
+      <p className=" font-thin font-Inter text-wrap w-11/12 pl-52 py-9">
+        I'm a passionate software developer based in Melbourne, Australia.
+        Currently, I am honing my skills and expanding my knowledge at Swinburne
+        University of Technology. My journey into the world of software
+        development is fueled by a deep curiosity and a love for technology.
+        When I'm not coding, you can find me capturing moments through
+        photography or creating melodies with my music. Welcome to my portfolio,
+        where I showcase my projects and the creative solutions I've developed
+        along the way.
+      </p>
     </main>
   );
 }
