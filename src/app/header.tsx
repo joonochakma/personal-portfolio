2f052080cbf95c2422096230f60bee5ee34d4ffb
Joono Chakma <73777352+joonopohr@users.noreply.github.com>
2024-06-12 23:50:07 +1000

    update intro paragraph

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -2,7 +2,7 @@ import Navlinks from './navlinks';
 
 function Header() {
   return (
-    <div className="font-Inter flex text-3xl">
+    <div className="font-Inter font-thin flex text-2xl">
       <h1 className="font-Inter flex-1 pl-52">JC.</h1>
       <Navlinks />
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,13 @@ export default function Home() {
       <Header />
       Hey. Im
       <Navbar />
-      I'm a software enginer based on melbourne
+      I'm a passionate software developer based in Melbourne, Australia.
+      Currently, I am honing my skills and expanding my knowledge at Swinburne
+      University of Technology. My journey into the world of software
+      development is fueled by a deep curiosity and a love for technology. When
+      I'm not coding, you can find me capturing moments through photography or
+      creating melodies with my music. Welcome to my portfolio, where I showcase
+      my projects and the creative solutions I've developed along the way.
     </main>
   );
 }
