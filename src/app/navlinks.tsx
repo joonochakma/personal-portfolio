7ac91f9b7f6c2ad486778d06cf7b8bf9ef79fb1d
Joono Chakma <73777352+joonopohr@users.noreply.github.com>
2024-06-16 21:44:17 +1000

    add light svg

--- src/app/navlinks.tsx
+++ src/app/navlinks.tsx
@@ -1,13 +1,15 @@
 import Link from 'next/link';
+import LightIcon from './icons/lightIcon';
 
 export default function Navlinks() {
   return (
-    <ul className=" pr-16 grid grid-cols-3 gap-3 ">
+    <ul className=" pr-24 grid grid-cols-3 gap-3 ">
       <Link href="/Projects">Projects</Link>
       <Link href="/resume" target="_blank">
         Resume
       </Link>
       <Link href="/Contact">Contact</Link>
+      <LightIcon colour="white" />
     </ul>
   );
 }
