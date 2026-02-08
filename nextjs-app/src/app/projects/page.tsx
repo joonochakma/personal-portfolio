efec6e12f138c6cd089e238d27065792625fc0b1
Joono Chakma <73777352+joonopohr@users.noreply.github.com>
2026-02-08 23:50:57 +1100

    fix: image duplicates and source code exposure

--- nextjs-app/src/app/projects/page.tsx
+++ nextjs-app/src/app/projects/page.tsx
@@ -29,7 +29,7 @@ export default function Projects() {
                           alt={post.title}
                           width={300}
                           height={300}
-                          priority={true}
+                          priority={false}
                           className="rounded-2xl object-cover shadow-lg"
                         />
                         <div className="absolute inset-0" />
