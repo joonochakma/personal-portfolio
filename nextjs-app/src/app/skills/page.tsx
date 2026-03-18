e96f6fe5f75e161a2eee979b711cb4ef4609bd15
Joono Chakma <73777352+joonopohr@users.noreply.github.com>
2026-03-18 17:11:36 +1100

    feat: add JcLab page and fix test errors and packges

--- nextjs-app/src/app/skills/page.tsx
+++ nextjs-app/src/app/skills/page.tsx
@@ -1,9 +1,8 @@
-import Comingsoon from '../coming-soon';
 import Creds from '../lib/creds';
 
-const Resume = () => (
+const Skills = () => (
   <main>
     <Creds />
   </main>
 );
-export default Resume;
+export default Skills;
